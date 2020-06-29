11f6519046d4ca4f635c41c407df84dfb044481c
jonriber <49167364+jonriber@users.noreply.github.com>
2020-06-29 10:06:46 -0300

    jest install

--- react_firebase_1/src/components/App.test.js
+++ react_firebase_1/src/components/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDom from 'react-dom';
+// import { render } from '@testing-library/react';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+it('renders without crashing', ()=> {
+  const div = document.createElement('div');
+
+  //Creating a new Div and rendering App inside a Router
+  ReactDom.render(
+    <Router>
+      <App/>
+    </Router>,div
+  );
+  ReactDom.unmountComponentAtNode(div);
 });
